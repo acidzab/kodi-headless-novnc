d32a66c37611da0a3f4ab46805f0a215bb265fc2
acidzab <samuele.maoloni@gmail.com>
2025-11-01 18:11:32 +0100

    rimesso python 3.12

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ IMAGE_NAME = 'fhriley/kodi-headless-novnc'
 PLATFORMS = ['linux/amd64', 'linux/arm64', 'linux/arm/v7']
 CACHE = f'type=registry,ref={IMAGE_NAME}:'
 BUILDX = 'docker buildx build {build_args} --platform {platforms} {tags} --cache-to type=inline,mode=max {push} {load} {no_cache} .'
-PYTHON_VERSION = '3.13'
+PYTHON_VERSION = '3.12'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=f'Build {IMAGE_NAME}')
